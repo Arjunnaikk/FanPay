2bff9e518f77e3cfbb1f6658e5c4549878f5d1bc
Arjun <arjunnaik0402@gmail.com>
2024-06-24 13:49:00 +0530

    db

---
+++ app/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const {Schema, model} = mongoose;
+
+const UserSchema = new Schema({
+    email: { type: String, required: true},
+    name: { type: String},
+    username: { type: String, required: true},
+    ProfilePick: { type: String},
+    CoverPick: { type: String},
+    createdAt: { type: String, default:Date.now},
+    updatedAt: { type: String, default:Date.now},
+
+})
+export default mongoose.models.User || model("User", UserSchema)
