3c18e6223c8b5fb828051c26589ccba62eaddcae
Arjun <arjunnaik0402@gmail.com>
2024-06-30 01:03:01 +0530

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-     <footer className='bg-[#171625] text-white px-5 h-12 flex justify-center items-center'>
+     <footer className='bg-[#171625] text-white px-5 h-20 md:h-12 flex justify-center items-center'>
         <p className="text-center">Copyright &copy; Get me the Galaxy - All rights reserved!</p>
         </footer>
     </>
