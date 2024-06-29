e46c079f7dfd285d56de70ddd053aaf825a81406
Arjun <arjunnaik0402@gmail.com>
2024-06-30 00:59:16 +0530

    route

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils";
 import Payment from "@/app/models/Payment";
 import connectDb from "@/db/connectDb";
+import User from "@/app/models/User";
 
 export const POST = async (req) => {
     await connectDb()
@@ -18,14 +19,15 @@ export const POST = async (req) => {
     let p = await Payment.findOne({oid: razorpay_order_id})
     if(!p){
         return NextResponse.json({success:false,message:"Order Id not found"})
-        console.log("PAYment problem")
     }
 
-    let xx = validatePaymentVerification({order_id: razorpay_order_id, payment_id:razorpay_payment_id}, razorpay_signature, process.env.KEY_SECRET)
+    let user = await User.findOne({username: p.to_user})
+    const secret = user.razorpaySecret
+
+    let xx = validatePaymentVerification({order_id: razorpay_order_id, payment_id:razorpay_payment_id}, razorpay_signature, secret)
 
         if(xx){
             const updatedPayment = await Payment.findOneAndUpdate({oid: razorpay_order_id}, {done:true}, {new:true})
-            console.log("PAyment problem")
             return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_user}?paymentdone=true`)
         }
         else{
